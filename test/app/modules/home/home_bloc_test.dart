e7395ec762e479ee46c8c9481cfbbbcc44a483fe
Tiala <fabricio.thyala@gmail.com>
2020-03-17 03:26:04 -0300

    initial commit

---
+++ test/app/modules/home/home_bloc_test.dart
@@ -0,0 +1,17 @@
+import 'package:bloc_pattern/bloc_pattern_test.dart';
+import 'package:recicla/app/modules/home/home_module.dart';
+
+void main() {
+  initModule(HomeModule());
+
+
+  // setUp(() {
+  //     bloc = HomeModule.to.bloc<HomeBloc>();
+  // });
+
+  // group('HomeBloc Test', () {
+  //   test("First Test", () {
+  //     expect(bloc, isInstanceOf<HomeBloc>());
+  //   });
+  // });
+}
